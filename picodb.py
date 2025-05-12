47690bcdb86cfdd4f767890da0372455a9aed4bd
ajay-desai9 <ajay.desai509@gmail.com>
2025-05-12 14:23:24 -0400

    large commit - Implement ProtocolHandler for parsing and serializing RESP-style messages

--- picodb.py
+++ picodb.py
@@ -36,10 +36,66 @@ class ProtocolHandler(object):
             return self.handlers[first_byte](socket_file)
         except KeyError:
             raise CommandError('Bad Request')
+     
+    def handle_simple_string(self, socket_file):
+        return socket_file.readline().rstrip('\r\n')
+
+    def handle_error(self, socket_file):
+        return Error(socket_file.readline().rstrip('\r\n'))
+
+    def handle_integer(self, socket_file):
+        return int(socket_file.readline().rstrip('\r\n'))
+
+    def handle_string(self, socket_file):
+        # First read the length ($<length>\r\n).
+        length = int(socket_file.readline().rstrip('\r\n'))
+        if length == -1:
+            return None  # Case for null.
+        length += 2  # Includes the trailing \r\n in count.
+        return socket_file.read(length)[:-2]
+
+    def handle_array(self, socket_file):
+        num_elements = int(socket_file.readline().rstrip('\r\n'))
+        return [self.handle_request(socket_file) for _ in range(num_elements)]
+
+    def handle_dict(self, socket_file):
+        num_items = int(socket_file.readline().rstrip('\r\n'))
+        elements = [self.handle_request(socket_file)
+                    for _ in range(num_items * 2)]
+        return dict(zip(elements[::2], elements[1::2]))
 
     # Serialize the response data and send it to the client.
     def write_response(self, socket_file, data):
-        pass
+        #Turn Python objects into their serialized counterparts
+        buf = BytesIO()
+        self._write(buf, data)
+        buf.seek(0)
+        socket_file.write(buf.getvalue())
+        socket_file.flush()
+
+    def _write(self, buf, data):
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+
+        if isinstance(data, bytes):
+            buf.write('$%s\r\n%s\r\n' % (len(data), data))
+        elif isinstance(data, int):
+            buf.write(':%s\r\n' % data)
+        elif isinstance(data, Error):
+            buf.write('-%s\r\n' % error.message)
+        elif isinstance(data, (list, tuple)):
+            buf.write('*%s\r\n' % len(data))
+            for item in data:
+                self._write(buf, item)
+        elif isinstance(data, dict):
+            buf.write('%%%s\r\n' % len(data))
+            for key in data:
+                self._write(buf, key)
+                self._write(buf, data[key])
+        elif data is None:
+            buf.write('$-1\r\n')
+        else:
+            raise CommandError('unrecognized type: %s' % type(data))
     
 class Server(object):
     def __init__(self, host='127.0.0.1', port=31337, max_clients=64):
